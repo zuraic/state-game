7872a2d35a49613d4e9cc365b34c934bb6599778
Lethisa Putri <lethisaputri@gmail.com>
2021-10-22 21:07:15 +0700

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+answer_state = screen.textinput(title="What's another state name?")
+screen.exitonclick()
+
